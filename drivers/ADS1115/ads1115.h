f15088e67560043313cccaaf1df2246b26e30984
PawelPlewa <pawel@plewa.it>
2025-07-25 21:01:55 +0200

    Added the digital_to_analog function

--- drivers/ADS1115/ads1115.h
+++ drivers/ADS1115/ads1115.h
@@ -85,4 +85,5 @@
 #define DISABLE_COMPARATOR   (0b11) // default
 
 esp_err_t ads_init(void);
-esp_err_t ads_read(const uint8_t channel, const uint8_t relative_to, const double max_voltage_input, uint16_t *value);
+esp_err_t ads_read(const uint8_t channel, const uint8_t relative_to, const double max_voltage_input, int16_t *value);
+double digital_to_analog(const double vdd, const int16_t value);
